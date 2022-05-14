39338fd999c19e442c4f3fd0ae4e1d4b366fdd8c
madhu0200 <104727432+madhu0200@users.noreply.github.com>
2022-05-14 10:50:29 +0530

    Update date.py

--- date.py
+++ date.py
@@ -1,10 +1,3 @@
 import datetime
 x=datetime.datetime.now()
 print(x.strftime("%A %I:%M:%S %p %d-%m-%Y day of the year:%j week of the year:%U"))
-a=input("plane name:")
-if(a=="SPANTHIPHYLLUM"):
-    print("Yes - Spathiphyllum is the best plant ever!")
-elif(a=="spathiphyllum"):
-    print("No, I want a big Spathiphyllum!")
-else:
-    print("Spathiphyllum! Not [input]!")
